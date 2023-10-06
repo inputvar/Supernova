234cae2c2dd17fcb8597e8ba188bc4693ad138f3
VirajSindhwad <virajsindhwadd@gmail.com>
2023-10-07 00:51:03 +0530

    completed ebooks non audio

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,18 +1,17 @@
 import React from "react";
 
-const Cart = () => {
+const Cart = ({imagePath, index, bookTitle}) => {
 
-  const temp = "https://www.smartprix.com/bytes/wp-content/uploads/2022/11/Naruto.jpg"
-
+  const bookImgPath = ""
   return  <div>
   <div class="col">
-  <div className="card">
-    <img src={temp} className="card-img-top" alt="..." style={{ objectFit: "contain" }} />
+  <div className="card" style={{ height: "600px"}}>
+    <img key={index} src={imagePath} className="card-img-top" alt="..." style={{ objectFit: "cover", height:"100%", width:"100%" }} />
     <div className="card-body">
-      <h5 className="card-title">Card title</h5>
+      <h5 className="card-title">{bookTitle}</h5>
       <div className="cart_btns">
-      <a href={temp} className="btn btn-outline-dark">English</a>
-      <a href={temp} className="btn btn-outline-dark">हिन्दी</a>
+      <a href={bookImgPath} className="btn btn-outline-dark btn-lg">English</a>
+      <a href={bookImgPath} className="btn btn-outline-dark btn-lg">हिन्दी</a>
     </div></div>
   </div>
   </div>
--- frontend/src/components/EBooks.js
+++ frontend/src/components/EBooks.js
@@ -1,21 +1,64 @@
 import React from 'react'
 import Cart from './Cart'
+import ancientDawn from '../images/books/ancient_dawn.jpeg';
+import boatBehind from '../images/books/boat_behind.jpeg';
+import bookTitle from '../images/books/book_title.jpeg';
+import climbHigh from '../images/books/Climb_high,jump_with_better_view.jpeg';
+import getItTogether from '../images/books/get_it_together.jpeg';
+import greatGatsby from '../images/books/great_gatsby.jpeg';
+import iAmEnough from '../images/books/i_am_enough.jpeg';
+import manifest from '../images/books/manifest.jpeg';
+import modernSpaces from '../images/books/modern_spaces.webp';
+import outOfTheWall from '../images/books/out_of_the_wall.jpeg';
+import payToYourselfFirst from '../images/books/pay_to_yourself_first.jpeg';
+import soThisIsUnfortunate from '../images/books/so_this_is_unfortunate.jpeg';
+
 
 function EBooks() {
 
-    const temp = "https://www.smartprix.com/bytes/wp-content/uploads/2022/11/Naruto.jpg"
+    const imagePaths = [
+        ancientDawn,
+        boatBehind,
+        bookTitle,
+        climbHigh,
+        getItTogether,
+        greatGatsby,
+        iAmEnough,
+        manifest,
+        modernSpaces,
+        outOfTheWall,
+        payToYourselfFirst,
+        soThisIsUnfortunate,
+      ];
 
+      const bookTitles = [
+        'Ancient Dawn',
+        'Boat Behind',
+        'Book Title',
+        'Climb High, Jump with a Better View',
+        'Get It Together',
+        'The Great Gatsby',
+        'I Am Enough',
+        'Manifest',
+        'Modern Spaces',
+        'Out of the Wall',
+        'Pay Yourself First',
+        'So This Is Unfortunate',
+      ];
+
+      const returnBookTitle = (bookTitles, index) => { 
+        return bookTitles[index]
+       }
+    
+    
   return (
     <div>
     <h1 className='fw-7 ebookSection'>READ, पढ़ने के लिए</h1>
     <div className='ebooksCards row row-cols-3 row-cols-md-4 g-4'>
-    <Cart/>
-    <Cart/>
-    <Cart/>
-    <Cart/>
-    <Cart/>
-    <Cart/>
-    <Cart/></div></div>
+    {imagePaths.map((imagePath, index) => (
+        <Cart imagePath={imagePath} index={index} bookTitle={returnBookTitle(bookTitles, index)}/>
+    ))}
+    </div></div>
   )
 }
 
