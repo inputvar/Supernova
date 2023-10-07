f9e0ba4c67f7c0808d94a1faa29b22c305ff59ee
VirajSindhwad <virajsindhwadd@gmail.com>
2023-10-07 08:30:34 +0530

    recent changes

--- frontend/src/components/CourseDetails.js
+++ frontend/src/components/CourseDetails.js
@@ -3,7 +3,58 @@ import courseData from './courseData'
 
 function CourseDetails() {
   return (
-    <div><h1>{courseData[0].title}</h1></div>
+    <div><h1>{courseData[0].title}</h1>
+    <img src={courseData[0].image}  alt='...'></img>
+    <p>Course Objectives:
+
+1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+3. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+4. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+5. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Week 1: Introduction to Content Writing
+
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Week 2-3: Writing Fundamentals
+
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Week 4-5: Understanding Online Content
+
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Week 6-7: Research Skills
+
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Week 8-9: Building Engaging Content
+
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Week 10-11: Freelance Writing and Career Opportunities
+
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+Week 12: Final Projects and Career Preparation
+
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+- Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
 
   )
 }
