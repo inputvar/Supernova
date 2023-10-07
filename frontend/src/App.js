a5261318fa22be9b7b2a7cab98513e0bee9c5132
Varun Kargathara <varunkargathara67@gmail.com>
2023-10-07 08:09:50 +0530

    Merge branch 'feature03' into feature01

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,10 @@ import { Home, SingleCourse, Cart } from "./pages";
 import Navbar from "./components/Navbar";
 import MentalHealth from "./components/MentalHealth";
 import ConnectVideo from "./components/ConnectVideo";
-<<<<<<< HEAD
 import Form from "./pages/Form";
 import EBooks from "./components/EBooks";
-<<<<<<< HEAD
-import Recommend from "./components/recommend";
-=======
 import CoursesPage from "./pages/CoursesPage";
-=======
-import HomePage from "./pages/Home/Index";
-import RoomPage from "./Room";
->>>>>>> feature03
 
->>>>>>> origin/feature01
 function App() {
   return (
     <BrowserRouter>
@@ -24,22 +15,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courses/:id" element={<SingleCourse />} />
-<<<<<<< HEAD
-        <Route path="/courses/Recommend" element={<Recommend/>} />
-        <Route path="/category/:category" element={<Courses />} />
-=======
->>>>>>> origin/feature01
         <Route path="/cart" element={<Cart />} />
         <Route path="/mentalHealth" element={<MentalHealth />} />
-<<<<<<< HEAD
         <Route path="/connect" element={<ConnectVideo />} />
         <Route path="/books" element={<EBooks />} />
         <Route path="/form" element={<Form />} />
         <Route path="/coursePage" element={<CoursesPage />} />
-=======
-        <Route path="/connect" element={<HomePage />} />
-        <Route path="/connect/room/:roomId" element={<RoomPage />}></Route>
->>>>>>> feature03
       </Routes>
     </BrowserRouter>
   );
